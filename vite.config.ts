0f449d1cd58740302c892b9b93a77c449d93fd6c
cosucosuudu <70382245+cosucosuudu@users.noreply.github.com>
2024-11-10 20:56:48 +0100

    Update vite.config.ts
    Added the server IP, since it was not accessible by default
    
    server: {
            host: '127.0.0.1',  // Add this to force IPv4 only
        },

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        host: '127.0.0.1'
+    },
     plugins: [
         react(),
         visualizer({
