6daad5e1bc5c8146c3a0f12fea7185d83e7885d6
cosucosuudu <70382245+cosucosuudu@users.noreply.github.com>
2024-11-10 21:11:18 +0100

    Update vite.config.ts
    Removed the server entry, because it didn't help
    
    server: {
            host: '127.0.0.1'
        },

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    server: {
-        host: '127.0.0.1'
-    },
     plugins: [
         react(),
         visualizer({
